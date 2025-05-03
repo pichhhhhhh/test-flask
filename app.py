da61c4c1b9add6c7edef9452a6abcd8c9636863d
pichhhhhhh <maosreypich771@gmail.com>
2025-05-03 13:18:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from Flask import flask
+from Flask import Flask
 app = Flask(name)
 @app.route('/')
 def index():
   return "<h1>Hello Welcome to Flask</h1>"
 
-if name == 'main":
+if name == 'main':
   app.run(host='0.0.0.0' , port='5000')
